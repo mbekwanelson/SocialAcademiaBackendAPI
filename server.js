f19fc0ec794dc5c05b9a4b88b2f6dff2a3813e01
Songeziwe <songeziwe.soboois@tagmarshal.com>
2023-08-27 11:00:51 +0200

    dummy file for refence

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Hello Good sir, I am living the dream");
+const 
+  express = require("express"),
+  app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    name: "Home Page"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Serve listening on port ${port}`)
+})
